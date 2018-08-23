8d09b6a61bbf8d978508834419201f8c5bd0f8b5
JakeW <jakewellsd@gmail.com>
2018-08-23 21:04:31 +0100

    add container model that checks if a container number is valid

---
+++ app/src/main/java/containersearch/jaked/containersearch/ContainerNumber.java
@@ -0,0 +1,63 @@
+package containersearch.jaked.containersearch;
+
+public class ContainerNumber {
+
+    String mContainerNumber;
+
+    public ContainerNumber(String containerNumber){
+        mContainerNumber = containerNumber;
+    }
+
+
+    public Boolean isValid(){
+        System.out.println(mContainerNumber);
+        String[] containerNumber = mContainerNumber.split("(?!^)");
+
+        if(containerNumber.length != 11){
+            return false;
+        }
+
+        for(int i = 0; i <= 3; i++){
+            if(!Character.isLetter(containerNumber[i].charAt(0))){
+                return false;
+            }
+        }
+        for(int i = 4; i <= 9; i++){
+            if(!Character.isDigit(containerNumber[i].charAt(0))){
+                return false;
+            }
+        }
+
+        int finalDigit = sumOfNumbers(containerNumber) % 11;
+
+        System.out.println(containerNumber[10] + " " + String.valueOf(finalDigit));
+        if(containerNumber[10].equals(String.valueOf(finalDigit))){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    private int sumOfNumbers(String[] containerNumber){
+        int total = 0;
+        for(int i =0; i <= 9; i++){
+            double a = Math.pow(2,i);
+            total += CharacterValue(containerNumber[i].charAt(0)) *  Math.pow(2,i);
+        }
+        return total;
+    }
+
+    private int CharacterValue(Character c){
+        if(Character.isDigit(c)){
+            return Integer.valueOf(c.toString());
+        }
+        else {
+            c = Character.toLowerCase(c);
+            int value = ((int) c ) - 87;
+            value += value / 11;
+            return value;
+        }
+    }
+}
