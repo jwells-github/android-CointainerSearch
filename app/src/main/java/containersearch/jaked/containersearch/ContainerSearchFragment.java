d0efef7dd7a7a4f0ff9b3e237f248c417b1e4ec9
JakeW <jakewellsd@gmail.com>
2018-08-28 21:18:56 +0100

    add main fragments and the navigation between fragments

---
+++ app/src/main/java/containersearch/jaked/containersearch/ContainerSearchFragment.java
@@ -0,0 +1,36 @@
+package containersearch.jaked.containersearch;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class ContainerSearchFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_container_search,container,false);
+        final Button btSubmit = v.findViewById(R.id.btSubmit);
+
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new SearchServicesFragment();
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return v;
+    }
+}
--- app/src/main/java/containersearch/jaked/containersearch/MainActivity.java
+++ app/src/main/java/containersearch/jaked/containersearch/MainActivity.java
@@ -1,13 +1,60 @@
 package containersearch.jaked.containersearch;
 
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_fragment);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        ContainerSearchFragment containerSearchFragment = new ContainerSearchFragment();
+        fragmentTransaction.add(R.id.fragment_container, containerSearchFragment).commit();
+
     }
+
 }
+
+/*
+
+    setContentView(R.layout.fragment_webview);
+    final WebView mWebview = (WebView) findViewById(R.id.webView);
+        System.out.println("VALUE OF a "  + (int) 'a');
+                System.out.println("VALUE OF c "  + (int) 'c');
+
+                ContainerNumber containerNumber = new ContainerNumber("HMCU1090544");
+                System.out.println(containerNumber.isValid());
+                mWebview.getSettings().setJavaScriptEnabled(true);
+                mWebview.loadUrl("https://www.hmm21.com/cms/business/ebiz/trackTrace/trackTrace/index.jsp?numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&is_quick=Y&quick_params=&type=2&number=HDMU5584547&submit.x=21&submit.y=10");
+                mWebview.setFindListener(new WebView.FindListener() {
+@Override
+public void onFindResultReceived(int i, int i1, boolean b) {
+        if(b){
+        System.out.println("Matches " + i1);
+        if(i1 > 0){
+        mWebview.setVisibility(View.VISIBLE);
+        }
+        }
+
+        }
+        });
+
+        ;
+        mWebview.setWebViewClient(new WebViewClient(){
+@Override
+public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        view.findAllAsync("Tracking Results");
+        }
+        });*/
